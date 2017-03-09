01f19fd0eba2cd445628dd93948a5c5b4118f7d2
awokelee <604326541@qq.com>
2017-03-09 14:52:15 +0800

    完成状态切换展示、清除已删除

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@
    */
   angular
     .module('todoApp', [])
-    .controller('TodoController', ['$scope', TodoController]);
+    .controller('TodoController', ['$scope', '$location', TodoController]);
 
 
-  function TodoController($scope) {
+  function TodoController($scope, $location) {
     // 1. 展示todo列表
     var vm = $scope;
 
@@ -47,25 +47,78 @@
           vm.taskList.splice(i, 1);
         }
       }
-    }; 
+    };
 
     // 4. 修改任务
     vm.editId = 0;
-    vm.edit = function (id) {
+    vm.edit = function(id) {
       vm.editId = id;
     };
 
-    vm.update = function () {
+    vm.update = function() {
       vm.editId = 0;
     };
 
     // 全选、反选
     vm.allChecked = false;
-    vm.checkAll = function () {
-      vm.taskList.forEach( function(task) {
+    vm.checkAll = function() {
+      vm.taskList.forEach(function(task) {
         task.isCompleted = vm.allChecked;
       });
-    }
+    };
+
+    // 6 清除已经完成任务
+    vm.clearAll = function() {
+      var temp = [];
+      for (var i = 0; i < vm.taskList.length; i++) {
+        if (!vm.taskList[i].isCompleted) {
+          temp.push(vm.taskList[i]);
+        }
+      }
+      vm.taskList = temp;
+    };
+
+    // 显示隐藏已完成项
+    vm.isShow = function() {
+      var temp = false;
+
+      for (var i = 0; i < vm.taskList.length; i++) {
+        if (vm.taskList[i].isCompleted) {
+          temp = true;
+          break;
+        }
+      }
+      return temp;
+    };
+
+    // 7. 显示未完成任务数
+    vm.geUnCompleted = function() {
+      var count = 0;
+
+      vm.taskList.forEach(function(task) {
+        if (!task.isCompleted) {
+          count++;
+        }
+      });
+      return count;
+    };
+
+    // 8. 根据 hash 切换
+    vm.selectedStatus = { isCompleted: undefined };
+    vm.location = $location;
+    vm.$watch('location.url()', function(newValue, oldValue) {
+      switch (newValue) {
+        case '/':
+          vm.selectedStatus = { isCompleted: undefined };
+          break;
+        case '/active':
+          vm.selectedStatus = { isCompleted: false };
+          break;
+        case '/completed':
+          vm.selectedStatus = { isCompleted: true };
+          break;
+      }
+    });
 
 
   }
