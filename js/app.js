eb631de20f5eca6a42c42c58509d559faea1471c
awokelee <604326541@qq.com>
2017-03-09 11:03:33 +0800

    完成展示todos、添加todos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,51 @@
-(function (window) {
-	'use strict';
+(function(window) {
+  /**
+   *  Module
+   *
+   * Description
+   */
+  angular
+    .module('todoApp', [])
+    .controller('TodoController', ['$scope', TodoController]);
 
-	// Your starting point. Enjoy the ride!
+
+  function TodoController($scope) {
+    // 1. 展示todo列表
+    var vm = $scope;
+
+    var taskList = [
+      { id: 1, name: '吃饭', isCompleted: false },
+      { id: 2, name: '睡觉', isCompleted: false },
+      { id: 3, name: '打豆豆', isCompleted: true },
+      { id: 4, name: '撸啊撸', isCompleted: false }
+    ];
+
+    vm.taskList = taskList;
+
+    // 2. 添加任务
+    vm.newTask = '';
+    vm.add = function() {
+      if (vm.newTask.trim() === '') {
+        return;
+      }
+      // 设置 id
+      var id, len = vm.taskList.length;
+      if (len === 0) {
+        id = 1;
+      } else {
+        id = vm.taskList[len - 1].id + 1;
+      }
+
+      vm.taskList.push({id: id, name: vm.newTask, isCompleted: false});
+      vm.newTask = '';
+      
+      
+
+
+
+    };
+
+  };
+
 
 })(window);
